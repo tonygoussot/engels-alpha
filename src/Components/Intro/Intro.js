322fff62fe0976f2b4c030193fc432b6023e2b4f
tonygoussot <tonygoussot@users.noreply.github.com>
2018-04-07 16:40:52 +0200

    style(scss): adding materialize-css to improve css handling

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './Intro.scss';
-import '../../Style/card.scss';
 import {manifeste} from "./manifeste";
 
 class Intro extends Component {
@@ -14,25 +13,28 @@ class Intro extends Component {
 					     data-width="400"
 					     data-show-text="false">
 					</div>
+
 					<div className="Intro-networks card">
 						<div className="Intro-manifeste__title">
 							MOBILISONS-NOUS !
 							<div className="facebook--link">
-								<i class="fab fa-facebook-square"></i>
+								<i className="fab fa-facebook-square"></i>
 								<span> Rejoignez le mouvement </span>
 							</div>
 							<div>
-								<i class="fab fa-twitter-square"></i>
+								<i className="fab fa-twitter-square"></i>
 								<span> Suivez l'actualité twitter </span>
 							</div>
 						</div>
 					</div>
+
 					<div className="Intro-networks card">
 						<div className="Intro-manifeste__title ">
 							LE TRACT AUX CHEMINOT•E•S
 						</div>
 					</div>
-					<a class="twitter-timeline card"
+
+					<a className="twitter-timeline card"
 					   data-width="400"
 					   data-height="600"
 					   data-theme="light"
@@ -45,14 +47,13 @@ class Intro extends Component {
 				<div className="col-two">
 					<div className="Intro-manifeste card">
 						<div className="Intro-manifeste__image card-image">
-						</div>
-						<div className="Intro-manifeste__title card-title">
-							LE MANIFESTE
+							<img src={"/poing-levé.jpg"} />
+							<span className="card-title">LE MANIFESTE</span>
 						</div>
 						<div className="Intro-manifeste__texte card-content">
 							{manifeste}
 						</div>
-						<div className="Intro-manifeste__sign">
+						<div className="Intro-manifeste__sign card-action">
 							<a href={"https://www.change.org/p/le-peuple-appel-au-samedi-5-mai-faire-sa-f%C3%AAte-%C3%A0-macron"}
 							   className="sign__button ">
 								<i className="material-icons">assignment</i>
