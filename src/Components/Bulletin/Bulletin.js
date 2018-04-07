2e85fd2e7a7ba41187a1a362b2858c3fdf2fd276
tonygoussot <tonygoussot@users.noreply.github.com>
2018-04-07 19:49:24 +0200

    fix(global): fixed styling and created full responsive layout (hope so)

--- src/Components/Bulletin/Bulletin.js
+++ src/Components/Bulletin/Bulletin.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 import './Bulletin.scss'
+import {calcRes} from "../../Helpers/calcRes";
 
 class Bulletin extends Component {
+	constructor (props) {
+		super(props);
+		this.setState({videoRes: {w: 0, h: 0}});
+	}
+
+	componentWillMount () {
+		const resRatio    = 560 / 315;
+		const screenWidth = window.outerWidth;
+		this.setState(() => ({
+			videoRes: calcRes(screenWidth, resRatio, 800)
+		}));
+	}
+
 	render () {
 		return (
 			<div className="Bulletin">
-				<iframe width="800" height="450" src="https://www.youtube.com/embed/PJPi49pqDt8?rel=0" frameborder="0"
+				<iframe width={this.state.videoRes.w} height={this.state.videoRes.h}
+				        src="https://www.youtube.com/embed/PJPi49pqDt8?rel=0"
+				        frameborder="0"
 				        allow="autoplay; encrypted-media" allowfullscreen></iframe>
 			</div>
 		);
@@ -13,3 +29,4 @@ class Bulletin extends Component {
 }
 
 export default Bulletin;
+
--- src/Components/Comite/Comite.js
+++ src/Components/Comite/Comite.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import './Comite.scss'
+import {calcRes} from "../../Helpers/calcRes";
 
 class Comite extends Component {
+	constructor (props) {
+		super(props);
+		this.setState({frameRes: {w: 0, h: 0}});
+	}
+
+	componentWillMount () {
+		const resRatio    = 560 / 315;
+		const screenWidth = window.outerWidth;
+		this.setState(() => ({
+			frameRes: calcRes(screenWidth, resRatio)
+		}));
+	}
+
 	render () {
 		return (
-			<div>
-				<iframe src="https://www.google.com/maps/d/embed?mid=1LbB_kfMW6n_iKommVWPwmoLZELO6J0Vo" width="640" height="480"></iframe>
+			<div className="Comite">
+				
+				<iframe src="https://www.google.com/maps/d/embed?mid=1LbB_kfMW6n_iKommVWPwmoLZELO6J0Vo"
+				        width={this.state.frameRes.w}
+				        height={this.state.frameRes.h}>
+				</iframe>
 			</div>
 		);
 	}
